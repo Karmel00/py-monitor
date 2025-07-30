6c91c296995d0a8ba1aca65ff996f89ae3f6dca1
Karmel00 <karmeljjj.2005@wp.pl>
2025-07-30 15:08:49 +0000

    podstawowy monitor

--- monitor.py
+++ monitor.py
@@ -1,1 +1,13 @@
 import requests
+
+def web_response():
+    try:
+        response = requests.get("https://example.com")
+        if response.status_code == 200:
+            print("działa")
+        else:
+            print("bład")
+    except Exceptation as a:
+        print("bład:", a)
+
+web_response()
